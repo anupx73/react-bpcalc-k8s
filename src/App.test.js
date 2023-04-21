4a9c52f8b36118cf9c78e4f3207cd0681b154ddc
Anupam Saha <anupam.saha@clover.com>
2023-04-21 21:43:39 +0100

    feat: unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders BP Calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/BP Calculator/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Renders Systolic', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Systolic/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Renders Diastolic', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Diastolic/i);
   expect(linkElement).toBeInTheDocument();
 });
