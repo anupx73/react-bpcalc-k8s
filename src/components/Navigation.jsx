884c3d39ad52243752a53b0b79c93b60b06decbc
Anupam Saha <anupam.saha@clover.com>
2023-05-02 22:16:09 +0100

    ci: gke deploy

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ function Navigation() {
         <div class="container">
           <NavLink className="navbar-brand" to="/">
             <h4 class="text-primary">
-              <span class="fa-solid fa-droplet" />&nbsp;&nbsp;BP Category Calculator
+              <span class="fa-solid fa-droplet" />&nbsp;&nbsp;Upgraded BP Category Calc
             </h4>
           </NavLink>
           <div>
